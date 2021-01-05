bc8ae90f9adebc4eb1ca0733c56b489b306f65d4
zhumeilu <824434649@qq.com>
2021-01-05 19:02:55 +0800

    init

---
+++ src/main/java/com/zml/bookrent/BookRentApplication.java
@@ -0,0 +1,12 @@
+package com.zml.bookrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zml/bookrent/BookRentApplicationTests.java
@@ -0,0 +1,12 @@
+package com.zml.bookrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
